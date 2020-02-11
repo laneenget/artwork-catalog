f9153ad658f38014255de133a36889fa59224b67
Lane Enget <lane.enget@go.minneapolis.edu>
2020-02-11 17:48:43 -0600

    Define ui.display_menu function

--- ui.py
+++ ui.py
@@ -3,13 +3,23 @@
 def get_choice():
     display_menu()
     while True:
-        command = input('Command: ').upper()
+        command = input('Enter a key: ').upper()
         if is_valid(command):
             return command
         else:
             print('Not a valid choice, try again.')
 
 def display_menu():
+    print('')
+    print('Welcome to the Artwork Catalog!')
+    print('Enter a key from the menu to get started.')
+    print('1: Add a new artist')
+    print('2: Search for artwork by artist')
+    print('3: Search for available artwork by artist')
+    print('4: Add new artwork')
+    print('5: Delete artwork')
+    print('6: Change artwork availability')
+    print('Q: Quit')
 
 def is_valid():
 
