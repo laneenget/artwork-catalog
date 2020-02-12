3a329162064a282d0fb1151a462afde029a98187
Lane Enget <lane.enget@go.minneapolis.edu>
2020-02-11 18:22:19 -0600

    Define artstore.__init__ funtion

--- artstore.py
+++ artstore.py
@@ -1,6 +1,7 @@
 import sqlite3
+import os
 
-def main():
+db = os.path.join('catalog.db')
 
 class Art:
 
@@ -9,6 +10,16 @@ class ArtStore:
     class _ArtStore:
 
         def __init__():
+            create_artist_tbl = 'CREATE TABLE IF NOT EXISTS artist (artist TEXT, email TEXT, UNIQUE(artist COLLATE NOCASE, email COLLATE NOCASE))'
+            create_artwork_tbl = 'CREATE TABLE IF NOT EXISTS artwork (artist TEXT, title TEXT, price DECIMAL(10, 2), available BOOLEAN UNIQUE(title COLLATE NOCASE))'
+        
+            con = sqlite3.connect(db)
+        
+            with con:
+                con.execute(create_artist_tbl)
+                con.execute(create_artwork_tbl)
+
+            con.close()
 
         def _add_artist():
 
