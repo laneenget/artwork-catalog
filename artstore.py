36f54f87a6ad297a9b573a203ae56a557deffca3
Lane Enget <lane.enget@go.minneapolis.edu>
2020-02-12 09:21:59 -0600

    Define artworkdb.__init__ function

--- artstore.py
+++ artstore.py
@@ -22,13 +22,11 @@ class ArtistDB:
 
         def __init__():
             create_artist_tbl = 'CREATE TABLE IF NOT EXISTS artist (firstname TEXT, lastname TEXT, email TEXT, UNIQUE(firstname COLLATE NOCASE, lastname COLLATE NOCASE, email COLLATE NOCASE))'
-            create_artwork_tbl = 'CREATE TABLE IF NOT EXISTS artwork (artist TEXT, title TEXT, price DECIMAL(10, 2), available BOOLEAN UNIQUE(title COLLATE NOCASE))'
         
             con = sqlite3.connect(db)
         
             with con:
                 con.execute(create_artist_tbl)
-                con.execute(create_artwork_tbl)
 
             con.close()
 
@@ -41,7 +39,7 @@ class ArtistDB:
                     new_id = row.lastrowid  
                     artist.id = new_id  
             except sqlite3.IntegrityError as e:
-                raise BookError(f'This artist is already in the database: {artist.first_name} {artist.last_name}')
+                raise ArtstoreError(f'This artist is already in the database: {artist.first_name} {artist.last_name}')
             finally:
                 con.close()
 
@@ -51,9 +49,20 @@ class ArtworkDB:
 
     class _ArtworkDB:
         def __init__():
+            create_artwork_tbl = 'CREATE TABLE IF NOT EXISTS artwork (artist TEXT, title TEXT, price DECIMAL(10, 2), available BOOLEAN UNIQUE(title COLLATE NOCASE))'
+
+            con = sqlite3.connect(db)
+        
+            with con:
+                con.execute(create_artwork_tbl)
+
+            con.close()
 
         def _add_artwork():
 
         def _delete_artwork():
 
         def _update_artwork():
+
+class ArtstoreError(Exception):
+    pass
