57c67c590bd7abfc879c7e679865510699b620f8
Lane Enget <lane.enget@go.minneapolis.edu>
2020-02-13 13:28:02 -0600

    Add main.py functions

--- main.py
+++ main.py
@@ -1,4 +1,26 @@
-import ui
+from menu import Menu
 
 def main():
-    get_choice()
+    
+    menu = create_menu()
+
+def create_menu():
+    menu = Menu()
+    menu.add_command('1', 'Add artist', add_artist())
+    menu.add_command('2', 'Search for artwork by artist', search_artist())
+    menu.add_command('3', 'Display available artwork by artist', display_art())
+    menu.add_command('4', 'Add new artwork', add_artwork())
+    menu.add_command('5', 'Delete artwork', delete_artwork())
+    menu.add_command('6', 'Change artwork availability', change_availability())
+
+def add_artist():
+
+def search_artist():
+
+def display_artwork():
+
+def add_artwork():
+
+def delete_artwork():
+
+def change_availability():
--- ui.py
+++ ui.py
@@ -1,7 +1,6 @@
 
 
 def get_choice():
-    display_menu()
     while True:
         command = input('Enter a key: ').upper()
         if is_valid(command):
@@ -9,21 +8,8 @@ def get_choice():
         else:
             print('Not a valid choice, try again.')
 
-def display_menu():
-    print('')
-    print('Welcome to the Artwork Catalog!')
-    print('Enter a key from the menu to get started.')
-    print('1: Add a new artist')
-    print('2: Search for artwork by artist')
-    print('3: Search for available artwork by artist')
-    print('4: Add new artwork')
-    print('5: Delete artwork')
-    print('6: Change artwork availability')
-    print('Q: Quit')
-    print('')
-
 def is_valid():
-    
+
 
 #def show_artwork():
 
